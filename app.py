e5a177067649b05e66dd5e1ead5137d3a6c1a817
Gerard Nunes de Lima <gerard.lima@alliedbrasil.com.br>
2022-05-27 12:16:34 -0300

    adicionei infos

--- app.py
+++ app.py
@@ -1,2 +1,47 @@
-print("hello world")
+
+from distutils.log import debug
+from markupsafe import escape
+from flask import Flask
+from flask import url_for
+from flask import redirect, url_for
+
+
+app= Flask(__name__)
+
+
+@app.route("/")
+def menu():
+    return 'menu principal'
+
+@app.route("/nick/<nick>")
+def nick(nick):
+    return f'bem vindo, seu nick é: {escape(nick)}'
+
+
+@app.route('/login')
+def login():
+    return 'login'
+
+
+@app.route("/<name>")
+
+def hello(name):
+    return f'bem vindo user {escape(name)}!'
+
+
+if __name__== '__main__':
+    app.run(debug=True)
+
+@app.route('/admin/<adm>')
+def admin(adm):
+
+    if adm == "gerard":
+        return redirect (url_for("login"))
+    else:
+       return  redirect (url_for('login'))
+
+if __name__=='__main__':
+    app.run(debug=True)
+
+
 

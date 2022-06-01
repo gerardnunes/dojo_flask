4b55bc723d7e1a8a2873b94b18804bf0750ad7c7
Thiago Assis <thiago.medassis@gmail.com>
2022-06-01 10:44:28 -0300

    Mudanças no templates e no app.py

--- app.py
+++ app.py
@@ -1,35 +1,30 @@
-
-
-from distutils.log import debug
+import json
+from flask import Flask, request, render_template
 from markupsafe import escape
-from flask import Flask
-from flask import url_for
-from flask import redirect, url_for
-from flask import render_template
-
 
 app= Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        form = dict(request.form)
+        app.logger.info(f"{form}")
+        return render_template('login.html')
+
+    return render_template('login.html')
 
 
 @app.route("/teste/<numb>")
 def print(numb):
     return f'ola sua colisao sera em {numb}'
-  
-
-@app.route("/")
-def menu():
-    return render_template('index.html')
-    
 
 @app.route("/nick/<nick>")
 def nick(nick):
     return f'bem vindo, seu nick é: {escape(nick)}'
-
-
-@app.route('/menu', methods=['POST'])
-def login():
-    return render_template ('menu.html')
-
 
 @app.route("/ida/<name>")
 def hello(name):
